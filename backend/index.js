78d67de29fbe5076912599628fe5f1204988105f
Rushikesh <rvp6123@gmail.com>
2026-01-31 08:36:43 +0530

    Update a Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import {PORT, MONGODB_URL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
@@ -59,6 +59,28 @@ app.get('/books/:id', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 });
+//route for Update a book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if(!request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear"
+            });
+        }
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+        if(!result){
+            return response.status(404).send({message: "Book not found"});
+        }
+        return response.status(200).send({message: "Book updated successfully"});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 mongoose
     .connect(MONGODB_URL)
