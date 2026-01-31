3ac21e4c053f8778953da457f45e09e2eeb25018
Rushikesh <rvp6123@gmail.com>
2026-01-31 08:45:38 +0530

    Delete a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -81,6 +81,20 @@ app.put('/books/:id', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 })
+//route to get all books
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const result = await Book.findByIdAndDelete(id);
+        if(!result){
+            return response.status(404).send({message: "Book not found"});
+        }
+        return response.status(200).send({message: "Book deleted successfully"});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 mongoose
     .connect(MONGODB_URL)
