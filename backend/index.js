90c85b1f41be1969d5a6bc5f7f13a481a50c058a
Rushikesh <rvp6123@gmail.com>
2026-02-01 22:34:18 +0530

    show books list in react

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,14 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(
-    cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+//     })
+// );
 
 app.get('/', (request, response) =>{
     console.log(request);
